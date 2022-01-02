759994c118ae67acfce55df634e1c8763226e32f
Thomas Brasington <tom@2c2d.co.uk>
2022-01-02 11:09:34 +0000

    first

---
+++ app/routes/index.tsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "monospace, system-ui, sans-serif", lineHeight: "1.4",fontSize : 11, margin: 0, padding: 0, fontWeight: 400 }}>
+      <h1 style={{fontSize : 11, margin: 0, padding: 0, fontWeight: 400}}>2c2d</h1>
+      <p style={{margin: 0, padding: 0}}>welcome world</p>
+      <p style={{margin: 0, padding: 0}}><a href="https://www.tbrasington.com">Work</a></p>
+      <p style={{margin: 0, padding: 0}}><a href="https://www.thepostchaise.com">Photography</a></p>
+    </div>
+  );
+}
